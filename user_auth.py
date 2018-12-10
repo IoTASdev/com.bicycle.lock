4fb475c24fcae5798d2023a3679c186a2386f03a
IoTdev <iotqarancore@gmail.com>
2018-12-10 18:47:08 +0530

    Added auth based on JWT

--- user_auth.py
+++ user_auth.py
@@ -1,7 +1,6 @@
-
 import redis
 import json
-import pickle
+import jwt
 
 redis_client = connect = redis.Redis(host='localhost', port=6379, db=0) 
 
@@ -15,35 +14,36 @@ class User:
 		self.timestamp = 'NULL'
 
 	def register(self, user_name, password, mobile_no, email_id, timestamp, json_data):
-		self.user_name = user_name
-		self.password = password
-		self.mobile_no = mobile_no
-		self.email_id = email_id
-		self.timestamp = timestamp
-		srl_json_data = json.dumps(json_data)
-		user_data = {
+		if redis_client.exists(mobile_no) == False:
+			self.user_name = user_name
+			self.password = password
+			self.mobile_no = mobile_no
+			self.email_id = email_id
+			self.timestamp = timestamp
+			srl_json_data = json.dumps(json_data)
+			user_data = {
 						'consumer_type':'user',
 						'user_name':self.user_name,
 						'password':self.password,
 						'mobile_no':self.mobile_no,
 						'email_id':self.email_id,
 						'timestamp':self.timestamp
-					}
-		redis_client.hmset(self.mobile_no, user_data)
-		#redis_client.hset(self.mobile_no, 'consumer_type','user')
-		#redis_client.hset(self.mobile_no, 'user_name', self.user_name)
-		#redis_client.hset(self.mobile_no, 'password', self.password)
-		#redis_client.hset(self.mobile_no, 'mobile_no', self.mobile_no)
-		#redis_client.hset(self.mobile_no, 'email_id', self.email_id)
-		#redis_client.hset(self.mobile_no, 'timestamp', self.timestamp)
-		print(redis_client.hget(self.mobile_no, 'user_name'))
-		return 'user added succefully'
+						}
+			redis_client.hmset(self.mobile_no, user_data)
+			print(redis_client.hget(self.mobile_no, 'user_name'))
+			response = {'status':'RES_CREATED', 'message':'User Created Succefully'}
+			return response
+		else:
+			response = {'status':'BAD_REQUEST', 'message':'User already exists! Try with login'}
+			return response
 
 	def login(self, mobile_no, password):
 		if redis_client.exists(mobile_no) == True:
-			print(redis_client.hget(mobile_no,'password'))
-			if redis_client.hget(mobile_no,'password') == password:
-				return {'message':'Logged In Successfully','status': 'RES_CREATED'}
+			print(redis_client.hget(mobile_no,'password').decode("utf-8"))
+			if redis_client.hget(mobile_no,'password').decode("utf-8") == password:
+				message = {'mobile_no':mobile_no, 'password':password}
+				encoded_data = jwt.encode(message , 'secret', algorithm='HS256')
+				return {'message':'Logged In Successfully','status': 'RES_CREATED','Authorization':encoded_data.decode("utf-8")}
 			else:
 				return {'message':'Invalid Credentials','status': 'BAD_REQUEST'}
 		else:
