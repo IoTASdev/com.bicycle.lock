24b8c2ea1ed07f30de05da1e28abe2c0df9d96b8
IoTdev <iotqarancore@gmail.com>
2018-12-12 16:09:12 +0530

    Included stand create api in file
    1. Included API for stand create.
    2. Change in API structure (for user converted /provision to /provision/user and corresponding change in operation parameter in Header)

--- __init__.py
+++ __init__.py
@@ -1,38 +1,60 @@
 from flask import Flask, request, jsonify
 import user_auth
-import json
+import stand
 
-user = user_auth.User('NULL', 'NULL', 'NULL', 'NULL','NULL')
+user = user_auth.User('NULL', 'NULL', 'NULL', 'NULL', 'NULL')
+
+stand = stand.Stand('NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL')
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'I am on homepage'
 
-@app.route('/provision', methods = ['POST','GET'])
-def provision():
-	if request.method == 'POST':
-		if request.headers['Content-Type'] == 'application/json':
-			if request.headers['operation'] == 'createUser':
-				data = request.json
-				print(data['user_name'])
-				response = user.register(data['user_name'], data['password'], data['mobile_no'], data['email_id'], data['timestamp'], request.json)
-				if response['status'] == 'RES_CREATED':
-					return jsonify(response),201
-				else:
-					return jsonify(response),400
-			
-			elif request.headers['operation'] == 'loginUser':
-				data = request.json
-				response = user.login(data['mobile_no'], data['password']) 
-				if response['status'] == 'RES_CREATED':
-					return jsonify(response),201
-				else:
-					return jsonify(response),400
-		else:
-			response =  {'status': 'BAD_REQUEST', 'message':'Not Supported Content Type'}
-			return jsonify(response),400
+
+@app.route('/provision/user', methods=['POST', 'GET'])
+def provision_user():
+    if request.method == 'POST':
+        if request.headers['Content-Type'] == 'application/json':
+            if request.headers['operation'] == 'create':
+                data = request.json
+                print(data['user_name'])
+                response = user.register(data['user_name'], data['password'], data['mobile_no'], data['email_id'],
+                                         data['timestamp'])
+                if response['status'] == 'RES_CREATED':
+                    return jsonify(response), 201
+                else:
+                    return jsonify(response), 400
+
+            elif request.headers['operation'] == 'login':
+                data = request.json
+                response = user.login(data['mobile_no'], data['password'])
+                if response['status'] == 'RES_OK':
+                    return jsonify(response), 200
+                else:
+                    return jsonify(response), 400
+        else:
+            response = {'status': 'BAD_REQUEST', 'message': 'Not Supported Content Type'}
+            return jsonify(response), 400
+
+
+@app.route('/provision/stand', methods=['POST', 'GET'])
+def provision_stand():
+    if request.method == 'POST':
+        if request.headers['Content-Type'] == 'application/json':
+            if request.headers['operation'] == 'create':
+                data = request.json
+                response = stand.register(data['stand_name'], data['stand_coordinates'], data['stand_manager'], data['manager_contact'], data['no_of_bicycles'])
+                if response['status'] == 'RES_CREATED':
+                    return jsonify(response), 201
+                else:
+                    return jsonify(response), 400
+        else:
+            response = {'status': 'BAD_REQUEST', 'message': 'Not Supported Content Type'}
+            return jsonify(response), 400
+
 
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0')
+    app.run(host='0.0.0.0')
