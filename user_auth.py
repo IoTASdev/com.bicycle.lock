2917093d2e5b043273372bf1b0f2f1b7d03dd035
IoTdev <iotqarancore@gmail.com>
2018-12-12 16:12:22 +0530

    Optimized conditional check
    1. Removed uneccessary JSON import

--- user_auth.py
+++ user_auth.py
@@ -1,50 +1,49 @@
 import redis
-import json
 import jwt
 
-redis_client = connect = redis.Redis(host='localhost', port=6379, db=0) 
+redis_client = connect = redis.Redis(host='localhost', port=6379, db=0)
 
 
 class User:
 	def __init__(self, user_name, password, mobile_no, email_id, timestamp):
-		self.user_name = 'NULL'
-		self.password = 'NULL'
-		self.mobile_no = 'NULL'
-		self.email_id = 'NULL'
-		self.timestamp = 'NULL'
+		self.user_name = user_name
+		self.password = password
+		self.mobile_no = mobile_no
+		self.email_id = email_id
+		self.timestamp = timestamp
 
-	def register(self, user_name, password, mobile_no, email_id, timestamp, json_data):
-		if redis_client.exists(mobile_no) == False:
+	def register(self, user_name, password, mobile_no, email_id, timestamp):
+		print(redis_client.exists(mobile_no))
+		if redis_client.exists(mobile_no) is 0:
 			self.user_name = user_name
 			self.password = password
 			self.mobile_no = mobile_no
 			self.email_id = email_id
 			self.timestamp = timestamp
-			srl_json_data = json.dumps(json_data)
 			user_data = {
-						'consumer_type':'user',
-						'user_name':self.user_name,
-						'password':self.password,
-						'mobile_no':self.mobile_no,
-						'email_id':self.email_id,
-						'timestamp':self.timestamp
+						'consumer_type': 'user',
+						'user_name': self.user_name,
+						'password': self.password,
+						'mobile_no': self.mobile_no,
+						'email_id': self.email_id,
+						'timestamp': self.timestamp
 						}
 			redis_client.hmset(self.mobile_no, user_data)
 			print(redis_client.hget(self.mobile_no, 'user_name'))
-			response = {'status':'RES_CREATED', 'message':'User Created Succefully'}
+			response = {'status': 'RES_CREATED', 'message': 'User Created Successfully'}
 			return response
 		else:
-			response = {'status':'BAD_REQUEST', 'message':'User already exists! Try with login'}
+			response = {'status': 'BAD_REQUEST', 'message': 'User already exists! Try with login'}
 			return response
 
 	def login(self, mobile_no, password):
-		if redis_client.exists(mobile_no) == True:
-			print(redis_client.hget(mobile_no,'password').decode("utf-8"))
-			if redis_client.hget(mobile_no,'password').decode("utf-8") == password:
-				message = {'mobile_no':mobile_no, 'password':password}
-				encoded_data = jwt.encode(message , 'secret', algorithm='HS256')
-				return {'message':'Logged In Successfully','status': 'RES_CREATED','Authorization':encoded_data.decode("utf-8")}
+		if redis_client.exists(mobile_no) is 1:
+			print(redis_client.hget(mobile_no, 'password').decode("utf-8"))
+			if redis_client.hget(mobile_no, 'password').decode("utf-8") == password:
+				message = {'mobile_no': mobile_no, 'password': password}
+				encoded_data = jwt.encode(message, 'secret', algorithm='HS256')
+				return {'message': 'Logged In Successfully', 'status': 'RES_OK', 'Authorization': encoded_data.decode("utf-8")}
 			else:
-				return {'message':'Invalid Credentials','status': 'BAD_REQUEST'}
+				return {'message': 'Invalid Credentials', 'status': 'BAD_REQUEST'}
 		else:
-			return {'message':'User Does not exist','status': 'BAD_REQUEST'}
+			return {'message': 'User Does not exist', 'status': 'BAD_REQUEST'}
